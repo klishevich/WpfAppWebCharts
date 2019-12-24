1f89221e758faa80384b9880f16c6d18fb42b6fd
klishevich <busation@gmail.com>
2019-12-24 13:55:37 +0300

    sin and cos charts

--- WpfAppWebCharts/MainWindow.xaml.cs
+++ WpfAppWebCharts/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SciChart.Charting.Visuals;
+using SciChart.Charting.Model.DataSeries;
 
 namespace WpfAppWebCharts
 {
@@ -26,6 +27,22 @@ namespace WpfAppWebCharts
             InitializeComponent();
             Console.WriteLine("Licensing info: " + SciChartSurface.VersionAndLicenseInfo);
             Console.WriteLine("Dump info: " + SciChartSurface.DumpInfo());
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            // Create XyDataSeries to host data for our charts
+            var scatterData = new XyDataSeries<double, double>();
+            var lineData = new XyDataSeries<double, double>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                lineData.Append(i, Math.Sin(i * 0.1));
+                scatterData.Append(i, Math.Cos(i * 0.1));
+            }
+            // Assign dataseries to RenderSeries
+            LineSeries.DataSeries = lineData;
+            ScatterSeries.DataSeries = scatterData;
         }
     }
 }
