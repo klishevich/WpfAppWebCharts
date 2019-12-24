5fef891c6b3918ab3c0a2c4b05a7e2c4066cc1b6
klishevich <busation@gmail.com>
2019-12-24 13:15:52 +0300

    with code behind example

--- WpfAppWebCharts/MainWindow.xaml.cs
+++ WpfAppWebCharts/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SciChart.Charting.Visuals;
+using SciChart.Charting.Visuals.Axes;
+using SciChart.Charting.ChartModifiers;
+using SciChart.Charting.Visuals.Annotations;
 
 namespace WpfAppWebCharts
 {
@@ -26,6 +29,26 @@ namespace WpfAppWebCharts
             InitializeComponent();
             Console.WriteLine("Licensing info: " + SciChartSurface.VersionAndLicenseInfo);
             Console.WriteLine("Dump info: " + SciChartSurface.DumpInfo());
+            // Create the chart surface
+            // var sciChartSurface = new SciChartSurface();
+
+            // Create the X and Y Axis
+            var xAxis = new NumericAxis() { AxisTitle = "Number of Samples (per series)" };
+            var yAxis = new NumericAxis() { AxisTitle = "Value" };
+
+            sciChartSurface.XAxis = xAxis;
+            sciChartSurface.YAxis = yAxis;
+
+            // Specify Interactivity Modifiers
+            sciChartSurface.ChartModifier = new ModifierGroup(new RubberBandXyZoomModifier(), new ZoomExtentsModifier());
+            // Add annotation hints to the user
+            var textAnnotation = new TextAnnotation()
+            {
+                Text = "Hello World!",
+                X1 = 5.0,
+                Y1 = 5.0
+            };
+            sciChartSurface.Annotations.Add(textAnnotation);
         }
     }
 }
