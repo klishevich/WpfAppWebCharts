d706b014b23a37d802bf88563ab1395ddd9510cf
Michael klishevich <busation@gmail.com>
2020-11-30 17:20:01 +0300

    heatmap example

--- WpfAppWebCharts/MainWindow.xaml.cs
+++ WpfAppWebCharts/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SciChart.Charting.Visuals;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Charting.Model.DataSeries.Heatmap2DArrayDataSeries;
 
 namespace WpfAppWebCharts
 {
@@ -26,6 +28,30 @@ namespace WpfAppWebCharts
             InitializeComponent();
             Console.WriteLine("Licensing info: " + SciChartSurface.VersionAndLicenseInfo);
             Console.WriteLine("Dump info: " + SciChartSurface.DumpInfo());
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            // Then, in code-behind, apply the data
+            int cellHeight = 3;
+            int cellWidth = 4;
+            var data = new double[cellHeight, cellWidth];
+            for (int x = 0; x < cellWidth; x++)
+                for (int y = 0; y < cellHeight; y++)
+                {
+                    data[y, x] = (double)(x + y * cellWidth);
+                }
+            // TODO: Fill the data[,] array with values to represent your heat values
+            // NOTE: HeatmapColorPalette.Maximum and Minimum defines the mapping of gradient colors to data values
+
+            double startX = 0;
+            double stepX = 1;
+            double startY = 0;
+            double stepY = 1;
+            var heatmapDataSeries = new UniformHeatmapDataSeries<double, double, double>(data, startX, stepX, startY, stepY);
+
+            // Apply the dataseries to the heatmap
+            heatmapSeries.DataSeries = heatmapDataSeries;
         }
     }
 }
